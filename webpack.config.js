472c0ad87cca57beba5118ea4bd8cfba27b9f55a
AlaaAlsalem <alsalem.aladdin@gmail.com>
2021-12-03 08:28:05 +0200

    Add locale server

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+  mode: 'development',
   entry: './src/index.js',
 
   plugins: [
@@ -14,7 +15,9 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     clean: true,
   },
-
+  devServer: {
+    static: './dist',
+  },
   module: {
     rules: [
       {
