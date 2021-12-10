c6fefd81070b0c38ad2ed0a5c9115f367c5bc073
AlaaAlsalem <alsalem.aladdin@gmail.com>
2021-12-10 12:46:45 +0200

    Add jest

--- src/script.js
+++ src/script.js
@@ -1,13 +1,15 @@
 export const inputBox = document.querySelector('#add-bar');
 export const addBtn = document.querySelector('.enter');
-
 export const listArr = JSON.parse(localStorage.getItem('todos')) || [];
 export const createlocalstorage = (array) => {
   const localStorageObject = {
-    description: inputBox.value,
+    description: 'hey',//inputBox.value,
     completed: false,
     index: array.length + 1,
   };
   array.push(localStorageObject);
   localStorage.setItem('todos', JSON.stringify(array));
 };
+export const DummyFunction = (a,b) =>{
+  return a+b
+}
